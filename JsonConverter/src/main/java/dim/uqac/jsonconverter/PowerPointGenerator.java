dbababcd296f1b68b0f5c8d6d31bae427a697c95
Simon-Olivier Lachance <49047004+monstrotac@users.noreply.github.com>
2022-03-06 18:06:19 -0500

    BBada

--- JsonConverter/src/main/java/dim/uqac/jsonconverter/PowerPointGenerator.java
+++ JsonConverter/src/main/java/dim/uqac/jsonconverter/PowerPointGenerator.java
@@ -2,6 +2,8 @@ package dim.uqac.jsonconverter;
 
 import org.apache.poi.xslf.usermodel.*;
 
+import java.awt.*;
+
 public class PowerPointGenerator {
 
     //We start by inializing our variables.
@@ -16,7 +18,15 @@ public class PowerPointGenerator {
         defaultMaster = slideShow.getSlideMasters().get(0);
         layout = defaultMaster.getLayout(SlideLayout.TITLE_AND_CONTENT);
         XSLFSlide slide = slideShow.createSlide(layout);
+
         XSLFTextShape titleShape = slide.getPlaceholder(0);
         XSLFTextShape contentShape = slide.getPlaceholder(1);
+
+        XSLFTextBox shape = slide.createTextBox();
+        XSLFTextParagraph p = shape.addNewTextParagraph();
+        XSLFTextRun r = p.addNewTextRun();
+        r.setText("Baeldung");
+        r.setFontColor(Color.green);
+        r.setFontSize(24.);
     }
 }
