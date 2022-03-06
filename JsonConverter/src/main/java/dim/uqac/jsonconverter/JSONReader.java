6636415595006f37eb409d3d5bf9d1a779e5766d
NoahPM <NoahPlourdeM@hotmail.com>
2022-03-06 17:54:43 -0500

    Added stuff

--- JsonConverter/src/main/java/dim/uqac/jsonconverter/JSONReader.java
+++ JsonConverter/src/main/java/dim/uqac/jsonconverter/JSONReader.java
@@ -1,10 +1,35 @@
 package dim.uqac.jsonconverter;
 
-public class JSONReader {
-    private String jsonFile;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
-    public JSONReader (String fileName){
-
+public class JSONReader{
+    public JSONReader (){
+    }
+    //Read a JSON file and return an array
+    public JSONArray GetJSONDataFromFile(String fileName){
+        //Set the json parser
+        JSONParser jsonParser = new JSONParser();
+        //Set the return data
+        JSONArray data = null;
+        try (FileReader reader = new FileReader(fileName))
+        {
+            //Read JSON file
+            Object obj = jsonParser.parse(reader);
+            data = (JSONArray) obj;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return data;
     }
 
 }
