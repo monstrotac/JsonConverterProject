c158fcef1254f3a414d05fcb5f203b6ac693bea0
Simon-Olivier Lachance <49047004+monstrotac@users.noreply.github.com>
2022-03-06 16:52:07 -0500

    Maven dependency fix

--- JsonConverter/src/main/java/dim/uqac/jsonconverter/Main.java
+++ JsonConverter/src/main/java/dim/uqac/jsonconverter/Main.java
@@ -1,7 +1,20 @@
 package dim.uqac.jsonconverter;
 
+import org.apache.poi.xslf.usermodel.SlideLayout;
+import org.apache.poi.xslf.usermodel.XMLSlideShow;
+import org.apache.poi.xslf.usermodel.XSLFSlideLayout;
+import org.apache.poi.xslf.usermodel.XSLFSlideMaster;
+
 public class Main {
     public static void main(String[] args) {
+        //We start by inializing our variables.
+        XMLSlideShow powerPoint = new XMLSlideShow();
+        powerPoint.createSlide();
+
+        XSLFSlideMaster defaultMaster = powerPoint.getSlideMasters().get(0);
+        XSLFSlideLayout layout = defaultMaster.getLayout(SlideLayout.TITLE_AND_CONTENT);
+
+        //Here we construct our powerpoints.
 
     }
 }
